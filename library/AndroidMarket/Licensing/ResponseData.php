2c0648caa4d17e8ff56ccdae8976a24aef6a344a
dcaunt <dcaunt@9bbedade-4383-ce3b-2565-04fab2f84983>
2011-06-27 22:31:31 +0000

    Corrected LICENSE url
    git-svn-id: http://android-market-license-verification.googlecode.com/svn/trunk@3 9bbedade-4383-ce3b-2565-04fab2f84983

--- library/AndroidMarket/Licensing/ResponseData.php
+++ library/AndroidMarket/Licensing/ResponseData.php
@@ -5,7 +5,7 @@
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.
  * It is also available through the world-wide-web at this URL:
- * http://code.google.com/p/android-market-license-verification-php/source/browse/trunk/LICENSE
+ * http://code.google.com/p/android-market-license-verification/source/browse/trunk/LICENSE
  */
 
 /**
